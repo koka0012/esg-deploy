87d8a406b0cb48ea8e5bf7f9c9d6d437e220ae92
Gabriel Aparecido <gaap.gabriel.aparecido@gmail.com>
2024-06-24 15:27:21 -0300

    Criado a base para a página de login

--- app/app/auth/login/page.tsx
+++ app/app/auth/login/page.tsx
@@ -1,3 +1,77 @@
+"use client";
+
+import { Form, Formik, FormikHelpers } from "formik";
+import { object, string } from "yup";
+
+import Button from "@/app/components/Button/Button";
+import Input from "@/app/components/Input/Input";
+
+interface FormValues {
+  login: string;
+  password: string;
+}
+
 export default function page() {
-  return <div>login</div>;
+  const initialValues: FormValues = {
+    login: "",
+    password: "",
+  };
+
+  const validationSchema = object().shape({
+    login: string().required("é um campo obrigatório."),
+    password: string().required("é um campo obrigatório."),
+  });
+
+  const handleSubmit = async (
+    values: FormValues,
+    { setSubmitting }: FormikHelpers<FormValues>
+  ) => {
+    try {
+      console.log(values);
+    } catch (error) {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        validateOnBlur={false}
+        validateOnChange={false}
+        validateOnMount={false}
+        onSubmit={handleSubmit}
+      >
+        {({ values, handleChange, errors, isSubmitting }) => (
+          <Form>
+            <div className="p-4 flex flex-col gap-4">
+              <Input
+                name="login"
+                label="Login"
+                placeholder="Digite o login"
+                value={values.login}
+                handleChange={handleChange}
+                error={errors.login}
+              />
+
+              <Input
+                type="password"
+                name="password"
+                label="Senha"
+                placeholder="Digite a senha"
+                value={values.password}
+                handleChange={handleChange}
+                error={errors.password}
+              />
+
+              <Button type="submit" disabled={isSubmitting}>
+                Entrar
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
