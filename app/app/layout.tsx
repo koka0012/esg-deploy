540373a01c78d30eecde438fc1c5a9e15a4f79dd
Augusto Silveira <augustocezarchmieleski@gmail.com>
2024-06-27 18:17:58 -0400

    .

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,12 +1,15 @@
+import { login } from "../lib/api/login";
 import Header from "./components/layout/Header";
 import { Map } from "./components/map";
 
-export default function AppLayout({ children }: { children: React.ReactNode }) {
+export default async function AppLayout({ children }: { children: React.ReactNode }) {
+  const credentials = await login();
+
   return (
     <div className="flex flex-col h-screen overflow-hidden bg-cover">
       <Header />
       <div className="relative bg-opacity-0 bg-gray-900 inset-0 w-full h-full">
-        <Map />
+        <Map apiToken={credentials.token}/>
         <div className="relative h-full pointer-events-none">
           {children}
         </div>
