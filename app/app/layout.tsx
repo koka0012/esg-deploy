bf8483eb71312f8b9230f29abb405012f3460f81
Augusto Silveira <augustocezarchmieleski@gmail.com>
2024-06-27 11:28:16 -0300

    .

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,22 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import Header from "./components/layout/Header";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "ESG",
-  description: "",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <div className="flex flex-col h-screen overflow-hidden bg-cover bg-[url('../public/Images/bg_home.jpg')]">
+      <Header />
+
+      <div className="relative bg-opacity-0 bg-gray-900 inset-0 w-full h-full">
+        {children}
+      </div>
+    </div>
   );
 }
