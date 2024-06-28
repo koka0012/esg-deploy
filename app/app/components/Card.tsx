7bb72cbca7fc860a2b2d40967d31c2d66c7b3839
Augusto Silveira <augustocezarchmieleski@gmail.com>
2024-06-28 17:12:50 -0400

    .

--- app/app/components/Card.tsx
+++ app/app/components/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, content }) => {
   return (
-    <div className="flex flex-col bg-[#2a3042] rounded-md py-2 px-4 w-[25rem] h-72">
+    <div className="flex flex-col bg-[#2a3042] rounded-md py-2 px-4 w-[25rem] max-h-72">
       <h2 className="text-[#a2adcc] text-base font-thin font-publica mb-2">{title}</h2>
       <div className="text-zinc-400 overflow-auto flex-1">{content}</div>
     </div>
