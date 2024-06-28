a39a1ce853b1cba2f2e1d5f13b88f57dcd934f0d
EduardoSpectraX <eduardo.oliveira@spectrax.com.br>
2024-06-28 16:56:56 -0400

    feat: adiciona segunda tela do relatorios

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeInRight: {
+          "0%": { opacity: "0", transform: "translateX(20px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fadeInUp": "fadeInUp 0.3s ease-out",
+        "fadeInRight": "fadeInRight 0.3s ease-out",
       },
     },
   },
